3a60cc00df3d673241534df0b3d977942bd600c3
Tyler Crammond <tylerchc92@gmail.com>
2021-12-01 17:34:21 +0000

    Day one - part two

--- src/days/one.rs
+++ src/days/one.rs
@@ -1,5 +1,11 @@
 use crate::utils::read_input_lines_all;
 
+/*
+https://adventofcode.com/2021/day/1
+
+count the number of times the sum of measurements in this sliding window increases from the previous sum
+*/
+
 pub fn run() {
     let lines: Vec<i32> = read_input_lines_all("one.txt")
         .iter()
@@ -16,15 +22,22 @@ fn assess_depth(data: Vec<i32>) -> i32 {
     let mut increased_count = 0;
     let mut prev_value: Option<i32> = None;
 
-    for value in data {
-        if prev_value.is_some() {
-            if value > prev_value.unwrap() {
-                increased_count += 1;
-            }
+    data.iter().fold(vec![], |acc, value| {
+        let mut next = acc.clone();
+        next.push(value.to_owned());
+        if next.len() < 3 {
+            return next;
         }
 
-        prev_value = Some(value);
-    }
+        let sum = next.iter().sum::<i32>();
+        if prev_value.is_some() && sum > prev_value.unwrap() {
+            increased_count += 1;
+        }
+        prev_value = Some(sum);
+        next.remove(0);
+
+        next
+    });
 
     println!("The depth inrceased {} times", increased_count.to_string());
     increased_count
@@ -38,7 +51,7 @@ mod tests {
     fn it_works_with_example_data() {
         assert_eq!(
             assess_depth(vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263,]),
-            7
+            5
         );
     }
 }
